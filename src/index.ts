38327ee3a1ca64940d62133a96f774a595186f94
Leon Sauer <leon.sauer@haw-hamburg.de>
2024-01-31 11:15:05 +0100

    function head added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import { mockDb, mockFetch } from "./mocks";
 
-// mockDb: A function that mocks a database call. It returns an array of ads.txt entry string
-//
-// mockFetch: A function that mocks a fetch call to the ads.txt endpoint of a publisher.
-// It returns a string similar to Result.text()
-//
-//
-// Challenge: Create a function that compares entries from database and request. Return entries that are missing
-//
-//
+export function adsTxtChecker(fetch: string, dbArray: string[]): string[]{
+    //TODO: implement
+    return [];
+};
